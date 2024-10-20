f29aecfce632943b3fc947c804792e3e27fd01f5
Ravi <ravi.mak2003@gmail.com>
2024-10-20 19:34:25 +0530

    add loading before tasks fetching

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,11 +6,13 @@ import TaskList from './components/TaskList';
 function App() {
   const [tasks, setTasks] = useState([]);
   const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   const URI = `https://kaushalam-task-backend.onrender.com/`;
 
   useEffect(() => {
     fetchTasks();
+    // alert("The server hosted on Render may take up to 1 minute to wake up. Please wait...");
   }, []);
 
   const fetchTasks = async () => {
@@ -18,6 +20,7 @@ function App() {
       const response = await fetch(`${URI}api/tasks`);
       const data = await response.json();
       setTasks(data);
+      setLoading(false);
     } catch (error) {
       console.error('Error fetching tasks:', error);
     }
@@ -78,33 +81,38 @@ function App() {
   };
 
   return (
+    
     <div className="min-h-screen bg-gray-100 py-8 px-4 sm:px-6 lg:px-8 flex flex-col">
-      <div className="flex-grow">
-        <div className="max-w-3xl mx-auto">
-          <h1 className="text-3xl font-bold text-gray-900 mb-8 text-center">Todo List</h1>
-          {error && <div className="mb-4 text-red-600 text-center">{error}</div>}
-          <div className="bg-white shadow-lg rounded-lg overflow-hidden">
-            <div className="p-6">
-              <TaskForm onAddTask={addTask} />
+      {loading ? (<div className='text-lg text-center'>Loading...</div>) : (
+        <div>
+          <div className="flex-grow">
+            <div className="max-w-3xl mx-auto">
+              <h1 className="text-3xl font-bold text-gray-900 mb-8 text-center">Todo List</h1>
+              {error && <div className="mb-4 text-red-600 text-center">{error}</div>}
+              <div className="bg-white shadow-lg rounded-lg overflow-hidden">
+                <div className="p-6">
+                  <TaskForm onAddTask={addTask} />
+                </div>
+                <TaskList tasks={tasks} onUpdateTask={updateTask} onDeleteTask={deleteTask} />
+              </div>
             </div>
-            <TaskList tasks={tasks} onUpdateTask={updateTask} onDeleteTask={deleteTask} />
           </div>
-        </div>
+      
+          <footer className="text-center mt-8">
+            <p className="text-gray-600">
+              Made with <span className="text-red-600">❤️</span> by 
+              <a 
+                href="https://github.com/RaviJM" 
+                className="text-indigo-600 hover:underline ml-1"
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                RaviJM
+              </a>
+            </p>
+          </footer>
       </div>
-  
-      <footer className="text-center mt-8">
-        <p className="text-gray-600">
-          Made with <span className="text-red-600">❤️</span> by 
-          <a 
-            href="https://github.com/RaviJM" 
-            className="text-indigo-600 hover:underline ml-1"
-            target="_blank" 
-            rel="noopener noreferrer"
-          >
-            RaviJM
-          </a>
-        </p>
-      </footer>
+    )}
     </div>
   );
   
