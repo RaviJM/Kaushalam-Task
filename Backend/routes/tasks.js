e121a72dc773ab64cdd2876d1205160784ed64f0
Ravi <ravi.mak2003@gmail.com>
2024-10-20 16:58:25 +0530

    update deprecated remove method

--- Backend/routes/tasks.js
+++ Backend/routes/tasks.js
@@ -53,7 +53,8 @@ router.delete('/:id', async (req, res) => {
     const task = await Task.findById(req.params.id);
     if (!task) return res.status(404).json({ message: 'Task not found' });
 
-    await task.remove();
+    // await task.remove();
+    await Task.findByIdAndDelete(req.params.id);
     res.json({ message: 'Task deleted' });
   } catch (err) {
     res.status(500).json({ message: err.message });
